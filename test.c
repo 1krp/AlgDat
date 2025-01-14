958fd144d8dfd1072252e9cb1e97fa517abab19a
1krp <oscar.dalne@gmail.com>
2025-01-14 21:32:36 +0100

    more tjafs

--- test.c
+++ test.c
@@ -1,1 +1,18 @@
-int main
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int *b = (int*)malloc(sizeof(int)*10); 
+    int c = 2;
+    b[3] = 5;
+
+    printf("hello");
+    printf("hello");
+    printf("hello");
+    printf("hello");
+    printf("hello");
+    printf("hello");
+    printf("hello");
+    free(b);
+}
